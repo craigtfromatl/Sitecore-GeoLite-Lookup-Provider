e7192bf468ef670c4cd8a7e4765ebd68cd9618e3
Chris Auer <cja@buildabonfire.com>
2017-01-03 01:01:20 -0500

    Updated code for 8.2. Added nuget for Sitecore nuget.

--- LookupProvider/GeoLiteProvider.cs
+++ LookupProvider/GeoLiteProvider.cs
@@ -3,18 +3,21 @@ using Sitecore.Analytics.Lookups;
 using Sitecore.Configuration;
 using Sitecore.Diagnostics;
 using System;
-using System.Web;
+using System.Globalization;
+using Sitecore;
 using System.Web.Hosting;
+using Sitecore.Analytics.Model;
 
 namespace SharedSource.GeoLite.LookupProvider
 {
     public class GeoLiteProvider : LookupProviderBase
     {
+        [UsedImplicitly]
         public override WhoIsInformation GetInformationByIp(string ip)
         {
             #region Variable Declarations
 
-            const string ModuleName = "SharedSource.GeoLite";
+            const string moduleName = "SharedSource.GeoLite";
 
             WhoIsInformation information = new WhoIsInformation();
 
@@ -22,10 +25,10 @@ namespace SharedSource.GeoLite.LookupProvider
 
             Assert.ArgumentNotNull(ip, "ip");
 
-            string geoCityPath = Settings.GetSetting(ModuleName + ".DatabaseLocation.GeoLiteCity", "~/app_data/GeoLiteCity.dat");
+            string geoCityPath = Settings.GetSetting(moduleName + ".DatabaseLocation.GeoLiteCity", "~/app_data/GeoLiteCity.dat");
             
-            string vLogging = Settings.GetSetting(ModuleName + ".VerboseLogging", "false");
-            bool vLoggingResult = false;
+            string vLogging = Settings.GetSetting(moduleName + ".VerboseLogging", "false");
+            bool vLoggingResult;
             bool verboseLogging = bool.TryParse(vLogging, out vLoggingResult);
 
             if (!vLoggingResult)
@@ -37,10 +40,10 @@ namespace SharedSource.GeoLite.LookupProvider
             {
                 if (verboseLogging)
                 {
-                    Log.Info(ModuleName + ": Lookup Initialized", this);
-                    Log.Info(ModuleName + ": IP Address: " + ip, this);
-                    Log.Info(ModuleName + ": geoLitePath: " + geoCityPath, this);
-                    Log.Info(ModuleName + ": full path: " + HostingEnvironment.MapPath(geoCityPath), this);
+                    Log.Info(moduleName + ": Lookup Initialized", this);
+                    Log.Info(moduleName + ": IP Address: " + ip, this);
+                    Log.Info(moduleName + ": geoLitePath: " + geoCityPath, this);
+                    Log.Info(moduleName + ": full path: " + HostingEnvironment.MapPath(geoCityPath), this);
                 }
 
                 var lookUpService = new LookupService(HostingEnvironment.MapPath(geoCityPath), LookupService.GEOIP_STANDARD);
@@ -52,67 +55,67 @@ namespace SharedSource.GeoLite.LookupProvider
                     information.Country = location.countryCode ?? string.Empty;
                     if (verboseLogging)
                     {
-                        Log.Info(ModuleName + ": information.Country = " + information.Country, this);
+                        Log.Info(moduleName + ": information.Country = " + information.Country, this);
                     }
 
                     information.Region = location.regionName ?? string.Empty;
                     if (verboseLogging)
                     {
-                        Log.Info(ModuleName + ": information.Region = " + information.Region, this);
+                        Log.Info(moduleName + ": information.Region = " + information.Region, this);
                     }
 
                     information.City = location.city ?? string.Empty;
                     if (verboseLogging)
                     {
-                        Log.Info(ModuleName + ": information.City = " + information.City, this);
+                        Log.Info(moduleName + ": information.City = " + information.City, this);
                     }
 
                     information.PostalCode = location.postalCode ?? string.Empty;
                     if (verboseLogging)
                     {
-                        Log.Info(ModuleName + ": information.PostalCode = " + information.PostalCode, this);
+                        Log.Info(moduleName + ": information.PostalCode = " + information.PostalCode, this);
                     }
 
-                    information.Latitude = (location.latitude == 0) ? string.Empty : location.latitude.ToString();
+                    information.Latitude = location.latitude.Equals(0) ? 0 : location.latitude;
                     if (verboseLogging)
                     {
-                        Log.Info(ModuleName + ": information.Latitude = " + ((location.latitude == 0) ? string.Empty : location.latitude.ToString()), this);
+                        Log.Info(moduleName + ": information.Latitude = " + (location.latitude.Equals(0) ? string.Empty : location.latitude.ToString(CultureInfo.InvariantCulture)), this);
                     }
 
-                    information.Longitude = (location.longitude == 0) ? string.Empty : location.longitude.ToString();
+                    information.Longitude = location.longitude.Equals(0) ? 0 : location.longitude;
                     if (verboseLogging)
                     {
-                        Log.Info(ModuleName + ": information.Longitude = " + ((location.longitude == 0) ? string.Empty : location.longitude.ToString()), this);
+                        Log.Info(moduleName + ": information.Longitude = " + (location.longitude.Equals(0) ? string.Empty : location.longitude.ToString(CultureInfo.InvariantCulture)), this);
                     }
 
                     information.MetroCode = (location.metro_code <= 0) ? string.Empty : location.metro_code.ToString();
                     if (verboseLogging)
                     {
-                        Log.Info(ModuleName + ": information.MetroCode = " + ((location.metro_code <= 0) ? string.Empty : location.metro_code.ToString()), this);
+                        Log.Info(moduleName + ": information.MetroCode = " + ((location.metro_code <= 0) ? string.Empty : location.metro_code.ToString()), this);
                     }
 
                     information.AreaCode = (location.area_code <= 0) ? string.Empty : location.area_code.ToString();
                     if (verboseLogging)
                     {
-                        Log.Info(ModuleName + ": information.AreaCode = " + ((location.area_code <= 0) ? string.Empty : location.area_code.ToString()), this);
+                        Log.Info(moduleName + ": information.AreaCode = " + ((location.area_code <= 0) ? string.Empty : location.area_code.ToString()), this);
                     }
                 }
                 else
                 {
-                    Log.Info(ModuleName + ": IP location cannot be determined.  IP Address: " + ip, this);
-                    information.BusinessName = Settings.GetSetting(ModuleName + ".NoLocationText", "Not Available");
+                    Log.Info(moduleName + ": IP location cannot be determined.  IP Address: " + ip, this);
+                    information.BusinessName = Settings.GetSetting(moduleName + ".NoLocationText", "Not Available");
                 }
 
                 if (verboseLogging)
                 {
-                    Log.Info(ModuleName + ": IP Lookup Complete", this);
+                    Log.Info(moduleName + ": IP Lookup Complete", this);
                 }
 
                 return information;
             }
             catch (Exception ex)
             {
-                Log.Error(ModuleName + ": Exception occurred.  Exception: " + ex.Message, this);
+                Log.Error(moduleName + ": Exception occurred.  Exception: " + ex.Message, this);
             }
 
             return null;
